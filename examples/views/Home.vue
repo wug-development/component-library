d91357f9285789e9ff013d9a24dabfc6fad1cb47
wuguang <guangwu@roseonly.com>
2020-12-08 13:54:52 +0800

    创建组件库

---
+++ examples/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="btn" @click="show">点击</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    show () {
+      this.$toast('OK')
+    }
+  }
+}
+</script>
